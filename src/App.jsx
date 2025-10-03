b127b1061648e7458fed9903a8ef04303b262ff4
Chiboy Cristian <chiboycristiansibarani@gmail.com>
2025-10-03 12:05:28 +0700

    refactor(core): reorganize project structure for scalability

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import DarkModeToggler from "./components/ui/DarkModeToggler";
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <div className="flex flex-col gap-5 min-h-screen items-center justify-center px-4 py-8 bg-white dark:bg-gray-900 dark:text-white">
-      <div className=" font-bold text-2xl">Welcome, Developer Siladan !</div>
-
-      <div className="mt-5">
-        <DarkModeToggler />
-      </div>
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
