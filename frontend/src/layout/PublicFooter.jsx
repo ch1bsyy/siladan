30f49e87f23bb06e9061676d8b5684773e557555
Chiboy Cristian <chiboycristiansibarani@gmail.com>
2025-10-29 15:10:26 +0700

    feat(frontend): add PublicFooter

---
+++ frontend/src/layout/PublicFooter.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo-siladan.png";
+
+const PublicFooter = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-[#916610] dark:bg-[#053F5C] text-[#9FE7F5] dark:text-slate-300">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center">
+          {/* Logo & Copyright */}
+          <div className="flex flex-col items-center md:items-start text-center md:text-left">
+            <Link to="/" className="flex items-center mb-4">
+              <img
+                src={Logo}
+                alt="Siladan Logo"
+                className="w-20 h-20 object-contain"
+              />
+              <span className="text-xl xl:text-2xl font-bold text-white dark:text-white">
+                SILADAN
+              </span>
+            </Link>
+            <p className="text-sm xl:text-base">
+              &copy; {currentYear} SILADAN. All right reserved.
+            </p>
+          </div>
+
+          {/* Quick Links */}
+          <div className="text-center">
+            <h4 className="text-base xl:text-lg font-semibold mb-3 text-white dark:text-white">
+              Tautan Cepat
+            </h4>
+            <ul className="space-y-2 gap-1 text-sm xl:text-base grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 whitespace-nowrap">
+              <li>
+                <Link to="/about" className="hover:underline hover:text-white">
+                  Tentang Kami
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/knowledge-base"
+                  className="hover:underline hover:text-white"
+                >
+                  Pusat Informasi
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/privacy-policy"
+                  className="hover:underline hover:text-white"
+                >
+                  Kebijakan Privasi
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Contact Info */}
+          <div className="text-center md:text-right">
+            <h4 className="text-base xl:text-lg font-semibold mb-3 text-white dark:text-white">
+              Hubungi Kami
+            </h4>
+            <p className="text-sm xl:text-base">
+              Jl. Ketintang No.103, Surabaya, Jawa Timur
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default PublicFooter;
--- frontend/src/layout/PublicLayout.jsx
+++ frontend/src/layout/PublicLayout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import PublicNavbar from "./PublicNavbar";
+import PublicFooter from "./PublicFooter";
 
 const PublicLayout = () => {
   return (
-    <div className="bg-slate-50 dark:bg-slate-900 min-h-screen">
+    <div className="bg-slate-50 dark:bg-slate-900 min-h-screen flex flex-col">
       <PublicNavbar />
 
-      <main>
+      <main className="flex-grow">
         <Outlet />
       </main>
+
+      <PublicFooter />
     </div>
   );
 };
