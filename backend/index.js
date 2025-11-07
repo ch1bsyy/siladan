fd6b3fb741eb0b583ecfca9c6b529e1842289d8f
ShyninK <rezashynink@gmail.com>
2025-11-07 21:10:35 +0700

    update swagger

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import 'dotenv/config'; // otomatis memuat .env
+import 'dotenv/config';
+import swaggerDocs from './swagger/docs.js';
 import tiketRoutes from './src/routes/tiketRoutes.js';
 
 const app = express();
@@ -7,6 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/tiket', tiketRoutes);
+
+swaggerDocs(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- backend/src/routes/tiketRoutes.js
+++ backend/src/routes/tiketRoutes.js
@@ -9,4 +9,34 @@ router.post('/store', tiketController.store); // POST /users/store
 router.put('/update/:id', tiketController.update); // PUT /users/update/:id
 router.delete('/delete/:id', tiketController.destroy); // DELETE /users/delete/:id
 
+/**
+ * @swagger
+ * /tiket:
+ *   get:
+ *     summary: Ambil semua tiket
+ *     description: Endpoint untuk mendapatkan semua data tiket
+ *     responses:
+ *       200:
+ *         description: Sukses
+ */
+router.get('/', (req, res) => {
+  res.json({ message: 'Daftar tiket berhasil diambil' });
+});
+
+/**
+ * @swagger
+ * /tiket/:id:
+ *   get:
+ *     summary: Ambil semua tiket
+ *     description: Endpoint untuk mendapatkan semua data tiket
+ *     responses:
+ *       200:
+ *         description: Sukses
+ */
+router.get('/', (req, res) => {
+  res.json({ message: 'Daftar tiket berhasil diambil' });
+});
+
+
+
 export default router;
